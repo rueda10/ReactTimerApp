22160d354c73e57b531dde080bc4bc0629ecd321
Jaime Rueda <rueda10@Jaimes-MacBook-Pro.local>
2017-02-14 10:33:17 -0600

    Add navigation

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,8 @@ var {Route, Router, IndexRoute, hashHistory} = require("react-router");
 // var IndexRoute = require("react-router").IndexRoute
 // var hashHistory = require("react-router").hashHistory
 var Main = require("Main");
+var Timer = require("Timer");
+var Countdown = require("Countdown");
 
 // Load foundation
 require("style-loader!css-loader!foundation-sites/dist/css/foundation.min.css")
@@ -17,6 +19,8 @@ require("style-loader!css-loader!sass-loader!applicationStyles")
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
+      <Router path="countdown" component={Countdown}></Router>
+      <IndexRoute component={Timer}></IndexRoute>
     </Route>
   </Router>,
   document.querySelector("#app")
